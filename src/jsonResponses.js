f73d99091dd90c668795ecc6505c4110ce606cd5
Sky Wan <sxw8136@rit.edu>
2019-09-19 22:07:51 -0400

    add comments

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,14 +1,17 @@
+// sends an object based on mime type
 const respondJSON = (request, response, status, object, type) => {
   response.writeHead(status, { "Content-Type": type });
   response.write(object);
   response.end();
 };
 
+// function to show a success status code and what message to send
 const success = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "This is a successful response"
   };
 
+  // if the mime type is xml, send back a 200 status response with appropriate XML response
   if (acceptedTypes[0] === "text/xml") {
     let responseXML = "<response>";
     responseXML = `${responseXML} <message>${responseJSON.message}</message>`;
@@ -22,6 +25,7 @@ const success = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 200, responseObj, "application/json");
 };
 
+// function to show a bad request without correct parameters
 const badRequest = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "This request has the required parameters"
@@ -30,7 +34,9 @@ const badRequest = (request, response, params, acceptedTypes) => {
   let responseXML;
   let responseObj;
 
+  // if the mime type is xml
   if (acceptedTypes[0] === "text/xml") {
+    // if the parameter is not valid or is not equal to "true", send back the correct xml response
     if (!params.valid || params.valid !== "true") {
       responseJSON.message = "Missing valid query parameter set to true";
       responseJSON.id = "badRequest";
@@ -47,6 +53,7 @@ const badRequest = (request, response, params, acceptedTypes) => {
     return respondJSON(request, response, 200, responseXML, "text/xml");
   }
 
+  // if the mime type is json, check for the validity of the param and send back an appropriate response
   if (!params.valid || params.valid !== "true") {
     responseJSON.message = "Missing valid query parameter set to true";
     responseJSON.id = "badRequest";
@@ -57,6 +64,7 @@ const badRequest = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 200, responseObj, "application/json");
 };
 
+// function to show not found message
 const notFound = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "The page you are looking for was not found.",
@@ -75,7 +83,8 @@ const notFound = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 404, responseObj, "application/json");
 };
 
-// issue with loggedIn=yes
+// function that checks for loggedIn parameter and if loggedIn is not set to yes,
+// send back the appropriate response
 const unauthorized = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "You have successfully viewed the content."
@@ -111,6 +120,7 @@ const unauthorized = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 200, responseObj, "application/json");
 };
 
+// function to show a forbidden error
 const forbidden = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "You do not have access to this content.",
@@ -129,6 +139,7 @@ const forbidden = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 403, responseObj, "application/json");
 };
 
+// function to show internal server error
 const internal = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message: "Internal Server Error. Something went wrong.",
@@ -147,6 +158,7 @@ const internal = (request, response, params, acceptedTypes) => {
   return respondJSON(request, response, 500, responseObj, "application/json");
 };
 
+// function to display error for a page that has not been implemented yet
 const notImplemented = (request, response, params, acceptedTypes) => {
   const responseJSON = {
     message:
--- src/server.js
+++ src/server.js
@@ -1,52 +1,58 @@
-const http = require('http');
-const url = require('url');
-const query = require('querystring');
-const htmlHandler = require('./htmlResponses.js');
-const jsonHandler = require('./jsonResponses.js');
+const http = require("http");
+const url = require("url");
+const query = require("querystring");
+const htmlHandler = require("./htmlResponses.js");
+const jsonHandler = require("./jsonResponses.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+// object that contains url routes followed by specific functions
 const urlStruct = {
   GET: {
-    '/': htmlHandler.getIndex,
-    '/style.css': htmlHandler.getCSS,
-    '/success': jsonHandler.success,
-    '/badRequest': jsonHandler.badRequest,
-    '/unauthorized': jsonHandler.unauthorized,
-    '/forbidden': jsonHandler.forbidden,
-    '/internal': jsonHandler.internal,
-    '/notImplemented': jsonHandler.notImplemented,
-    notFound: jsonHandler.notFound,
-  },
+    "/": htmlHandler.getIndex,
+    "/style.css": htmlHandler.getCSS,
+    "/success": jsonHandler.success,
+    "/badRequest": jsonHandler.badRequest,
+    "/unauthorized": jsonHandler.unauthorized,
+    "/forbidden": jsonHandler.forbidden,
+    "/internal": jsonHandler.internal,
+    "/notImplemented": jsonHandler.notImplemented,
+    notFound: jsonHandler.notFound
+  }
 };
 
+// handles http requests
 const onRequest = (request, response) => {
   console.log(request.url);
+  // parse the url and grab the query parameters
   const parsedUrl = url.parse(request.url);
   const params = query.parse(parsedUrl.query);
 
   // grab the 'accept' headers and split them into an array
-  const acceptedTypes = request.headers.accept.split(',');
+  const acceptedTypes = request.headers.accept.split(",");
 
-  console.log('params', params);
-  console.log('acceptedTypes', acceptedTypes);
+  console.log("params", params);
+  console.log("acceptedTypes", acceptedTypes);
+
+  // check if the path name matches anything in the urlStruct
   if (urlStruct[request.method][parsedUrl.pathname]) {
     urlStruct[request.method][parsedUrl.pathname](
       request,
       response,
       params,
-      acceptedTypes,
+      acceptedTypes
     );
   } else {
     urlStruct[request.method].notFound(
       request,
       response,
       params,
-      acceptedTypes,
+      acceptedTypes
     );
   }
 };
 
+// start the server
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
